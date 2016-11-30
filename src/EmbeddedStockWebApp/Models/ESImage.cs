c1a5d1a0bea4fe3cd7e5822cd8c603a4fb489494
Victor Nielsen <victor.nielsen@hotmail.com>
2016-11-30 15:14:32 +0100

    added new project

---
+++ src/EmbeddedStockWebApp/Models/ESImage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmbeddedStockWebApp.Models
+{
+    public class ESImage
+    {
+        [Key]
+        public long ESImageId { get; set; }
+        [MaxLength(128)]
+        public string ImageMimeType { get; set; }
+        public byte[] Thumbnail { get; set; }
+        public byte[] ImageData { get; set; }
+    }
+
+}
