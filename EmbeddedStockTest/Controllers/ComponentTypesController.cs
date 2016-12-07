50365fec757f7ef735760130c12d95bf30db1c20
Victor Nielsen <victor.nielsen@hotmail.com>
2016-12-07 14:19:25 +0100

    Added new project

---
+++ EmbeddedStockTest/Controllers/ComponentTypesController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using EmbeddedStockTest.Models;
+using System.Data.Entity.Infrastructure;
+using EmbeddedStockTest.ViewModels;
+
+namespace EmbeddedStockTest.Controllers
+{
+    public class ComponentTypesController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: ComponentTypes
+        public ActionResult Index()
+        {
+
+            return View(db.ComponentTypes.ToList());
+        }
+
+        // GET: ComponentTypes/Details/5
+        public ActionResult Details(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ComponentType componentType = db.ComponentTypes.Find(id);
+            if (componentType == null)
+            {
+                return HttpNotFound();
+            }
+            return View(componentType);
+        }
+
+        // GET: ComponentTypes/Create
+        public ActionResult Create()
+        {
+            var componentType = new ComponentType();
+            PopulateAssignedCategoryData(componentType);
+            return View();
+        }
+
+        public void PopulateAssignedCategoryData(ComponentType componentType)
+        {
+            var allCategories = db.Categories;
+            var componentTypesCategories = new HashSet<int>(componentType.Categories.Select( c => c.CategoryId));
+            var viewModel = new List<CategoryViewModel>();
+            foreach (var category in allCategories)
+            {
+                viewModel.Add(new CategoryViewModel
+                {
+                    CategoryId = category.CategoryId,
+                    Name = category.Name,
+                    Assigned = componentTypesCategories.Contains(category.CategoryId)
+                });
+            }
+            ViewBag.Categories = viewModel;
+        }
+        
+        // POST: ComponentTypes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ComponentTypeId,ComponentName,ComponentInfo,Location,Status,Datasheet,ImageUrl,Manufacturer,WikiLink,AdminComment")] ComponentType componentType, string[] selectedCategories)
+        {
+            if (selectedCategories != null)
+            {
+                foreach (var category in selectedCategories)
+                {
+                    var categoryToAdd = db.Categories.Find(int.Parse(category));
+                    componentType.Categories.Add(categoryToAdd);
+                }
+
+            }
+
+            if (ModelState.IsValid)
+            {
+                db.ComponentTypes.Add(componentType);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(componentType);
+        }
+
+        // GET: ComponentTypes/Edit/5
+        public ActionResult Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ComponentType componentType = db.ComponentTypes.Find(id);
+            if (componentType == null)
+            {
+                return HttpNotFound();
+            }
+            return View(componentType);
+        }
+
+        // POST: ComponentTypes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ComponentTypeId,ComponentName,ComponentInfo,Location,Status,Datasheet,ImageUrl,Manufacturer,WikiLink,AdminComment")] ComponentType componentType)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(componentType).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(componentType);
+        }
+
+        // GET: ComponentTypes/Delete/5
+        public ActionResult Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ComponentType componentType = db.ComponentTypes.Find(id);
+            if (componentType == null)
+            {
+                return HttpNotFound();
+            }
+            return View(componentType);
+        }
+
+        // POST: ComponentTypes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(long id)
+        {
+            ComponentType componentType = db.ComponentTypes.Find(id);
+            db.ComponentTypes.Remove(componentType);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ EmbeddedStockTest/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmbeddedStockTest.Models
+{
+	public class Category
+	{
+        public Category()
+        {
+            ComponentTypes = new List<ComponentType>();
+        }
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+	    public ICollection<ComponentType> ComponentTypes { get; protected set; }
+	}
+}
